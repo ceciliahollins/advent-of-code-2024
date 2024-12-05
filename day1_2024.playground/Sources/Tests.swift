edfaec8878e77efaa4358b01403e304709803622
Cecilia Hollins <chollins@deloitte.com.au>
2024-12-05 20:22:54 +1100

    day 1 tests

--- day1_2024.playground/Sources/Tests.swift
+++ day1_2024.playground/Sources/Tests.swift
@@ -3,43 +3,39 @@ import XCTest
 public class Tests: XCTestCase {
     
     let exampleInput =  """
-                        MMMSXXMASM
-                        MSAMXMSMSA
-                        AMXSXMAAMM
-                        MSAMASMSMX
-                        XMASAMXAMM
-                        XXAMMXXAMA
-                        SMSMSASXSS
-                        SAXAMASAAA
-                        MAMMMXMMMM
-                        MXMXAXMASX
+                        3   4
+                        4   3
+                        2   5
+                        1   3
+                        3   9
+                        3   3
                         """
     
     func testPartOneExample() {
-        let expected = 18
-        let actual = ceresSearch(exampleInput)
+        let expected = 11
+        let actual = historianHysteria(exampleInput)
         
         XCTAssertEqual(expected, actual)
     }
     
-//    func testPartOne() {
-//        let expected = 0
-//        let actual = ceresSearch(input)
-//        
-//        XCTAssertEqual(expected, actual)
-//    }
-//    
-//    func testPartTwoExample() {
-//        let expected = 0
-//        let actual = ceresSearchPartTwo(exampleInput)
-//        
-//        XCTAssertEqual(expected, actual)
-//    }
-//    
-//    func testPartTwo() {
-//        let expected = 0
-//        let actual = ceresSearchPartTwo(input)
-//        
-//        XCTAssertEqual(expected, actual)
-//    }
+    func testPartOne() {
+        let expected = 1938424
+        let actual = historianHysteria(input)
+        
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testPartTwoExample() {
+        let expected = 31
+        let actual = historianHysteriaPartTwo(exampleInput)
+        
+        XCTAssertEqual(expected, actual)
+    }
+    
+    func testPartTwo() {
+        let expected = 22014209
+        let actual = historianHysteriaPartTwo(input)
+        
+        XCTAssertEqual(expected, actual)
+    }
 }
